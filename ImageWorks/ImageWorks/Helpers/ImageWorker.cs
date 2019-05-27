2d0bbd9f8140defda659fd3fca23a7c75b21df06
Vadim <vadim.table.pc@gmail.com>
2019-05-27 13:57:51 +0300

    WPF C# Image convert

---
+++ ImageWorks/ImageWorks/Helpers/ImageWorker.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageWorks.Helpers
+{
+    public static class ImageWorker
+    {
+        /// <summary>
+        /// Для стиску довільного фото у вкажаному розмірі
+        /// </summary>
+        /// <param name="originalPic">Вхідна фотка</param>
+        /// <param name="maxWidth">Максимальна ширина яку хочу получити</param>
+        /// <param name="maxHeight">Максиальна висота яка може бути</param>
+        /// <returns>Вертаю фотку уже готову, якщо щось пішло не так то буде NULL</returns>
+        public static Bitmap CreateImage(Bitmap originalPic, int maxWidth, int maxHeight)
+        {
+            try
+            {
+                int width = originalPic.Width;
+                int height = originalPic.Height;
+                int widthDiff = width - maxWidth;
+                int heightDiff = height - maxHeight;
+                bool doWidthResize = (maxWidth > 0 && width > maxWidth && widthDiff > heightDiff);
+                bool doHeightResize = (maxHeight > 0 && height > maxHeight && heightDiff > widthDiff);
+                if (doWidthResize || doHeightResize || (width.Equals(height) && widthDiff.Equals(heightDiff)))
+                {
+                    int iStart;
+                    Decimal divider;
+                    if (doWidthResize)
+                    {
+                        iStart = width;
+                        divider = Math.Abs((Decimal)iStart / maxWidth);
+                        width = maxWidth;
+                        height = (int)Math.Round((height / divider));
+                    }
+                    else
+                    {
+                        iStart = height;
+                        divider = Math.Abs((Decimal)iStart / maxHeight);
+                        height = maxHeight;
+                        width = (int)Math.Round(width / divider);
+                    }
+                }
+                using (Bitmap outBmp = new Bitmap(width, height, PixelFormat.Format24bppRgb))
+                {
+                    using (Graphics oGraphics = Graphics.FromImage(outBmp))
+                    {
+                        oGraphics.DrawImage(originalPic, 0, 0, width, height);
+                        return new Bitmap(outBmp);
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ImageWorks/ImageWorks/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using ImageWorks.Helpers;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageWorks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) " +
+                "| *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            if (dlg.ShowDialog() == true)
+            {
+                try
+                {
+
+                    var filePath = dlg.FileName;
+                    var image = System.Drawing.Image.FromFile(dlg.FileName);
+
+                    //вхідне фото для обробки
+                    var bmpOrigin = new System.Drawing.Bitmap(image);
+                    var imageName = Guid.NewGuid().ToString() + ".jpg";
+                    var imageSave = ImageWorker.CreateImage(bmpOrigin, 500, 500);
+                    if (imageSave == null)
+                    {
+                        throw new Exception("Проблема обробки фото");
+                    }
+                    else
+                    {
+                        SaveFileDialog imageFolder = new SaveFileDialog();
+                        dlg.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) " +
+                            "| *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+                        if (dlg.ShowDialog() == true)
+                        {
+                            string imageFolderSave = dlg.FileName;
+                            var imageSaveEnd = System.IO.Path.Combine(filePath, imageName);                        
+                            imageSave.Save(imageSaveEnd, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Щось пішло не так {ex.Message}");
+                }
+            }
+        }
+    }
+}
