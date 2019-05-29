9bdc11e7de2a617f16f0de401c5bcb8c76b66f94
Vadim <vadim.table.pc@gmail.com>
2019-05-29 19:12:18 +0300

    Course work

---
+++ AnimatedMenu1/MenuAnimation/Register.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace MenuAnimado1
+{
+    public partial class Register : Window
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnReg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+     
+    }
+}
